023bfb11a08ff9b5ff5c5d9633513aeee733926e
Vlad <tsvetkov_vlad@ukr.net>
2019-06-28 22:44:49 +0300

    Show all times above from time line

--- app/src/main/java/com/tsvietok/meteoradar/RadarTime.java
+++ app/src/main/java/com/tsvietok/meteoradar/RadarTime.java
@@ -1,9 +1,21 @@
 package com.tsvietok.meteoradar;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class RadarTime {
     public int timestamp;
     public int timeout;
     public Boolean locked;
     public Boolean is_down;
     public int[] times;
+
+    String[] getTime() {
+        String[] _times = new String[times.length];
+        for (int i = 0; i < times.length; i++) {
+            Date _time = new Date((long) times[i] * 1000);
+            _times[i] = new SimpleDateFormat("HH:mm").format(_time);
+        }
+        return _times;
+    }
 }
