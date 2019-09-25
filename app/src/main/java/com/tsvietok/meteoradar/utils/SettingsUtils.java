9e120edd07ebbba46ce6549af7426f6ba83a66f9
Vlad <tsvetkov_vlad@ukr.net>
2019-09-26 02:23:11 +0300

    Move save to settings func to separate class

---
+++ app/src/main/java/com/tsvietok/meteoradar/utils/SettingsUtils.java
@@ -0,0 +1,19 @@
+package com.tsvietok.meteoradar.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SettingsUtils {
+    private static final String PREFS_NAME = "com.tsvietok.meteoradar.preferences";
+
+    public static void saveIntSetting(Context context, String key, int value) {
+        SharedPreferences.Editor editor = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        editor.putInt(key, value);
+        editor.commit();
+    }
+
+    public static int getIntSetting(Context context, String key) {
+        SharedPreferences sharedPref = context.getSharedPreferences(PREFS_NAME, 0);
+        return sharedPref.getInt(key, 0);
+    }
+}
