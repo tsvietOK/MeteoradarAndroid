8b0db13a4a643b46a747c24855e7856facd987ca
Vlad <tsvetkov_vlad@ukr.net>
2019-10-18 01:19:44 +0300

    SettingsUtils: Use MODE_PRIVATE instead of 0

--- app/src/main/java/com/tsvietok/meteoradar/dev/utils/SettingsUtils.java
+++ app/src/main/java/com/tsvietok/meteoradar/dev/utils/SettingsUtils.java
@@ -7,24 +7,24 @@ public class SettingsUtils {
     private static final String PREF_NAME = "com.tsvietok.meteoradar.dev.preferences";
 
     public static void saveIntSetting(Context context, String key, int value) {
-        SharedPreferences.Editor editor = context.getSharedPreferences(PREF_NAME, 0).edit();
+        SharedPreferences.Editor editor = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE).edit();
         editor.putInt(key, value);
         editor.commit();
     }
 
     public static int getIntSetting(Context context, String key) {
-        SharedPreferences sharedPref = context.getSharedPreferences(PREF_NAME, 0);
+        SharedPreferences sharedPref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         return sharedPref.getInt(key, -1);
     }
 
     public static void saveBooleanSetting(Context context, String key, boolean value) {
-        SharedPreferences.Editor editor = context.getSharedPreferences(PREF_NAME, 0).edit();
+        SharedPreferences.Editor editor = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE).edit();
         editor.putBoolean(key, value);
         editor.commit();
     }
 
     public static Boolean getBooleanSetting(Context context, String key) {
-        SharedPreferences sharedPref = context.getSharedPreferences(PREF_NAME, 0);
+        SharedPreferences sharedPref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         return sharedPref.getBoolean(key, true);
     }
 }
