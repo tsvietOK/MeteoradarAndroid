f4a614e5177c5500080f91b41701e737765f40c1
Vlad <tsvetkov_vlad@ukr.net>
2019-10-17 19:13:51 +0300

    Fix preference name key

--- app/src/main/java/com/tsvietok/meteoradar/utils/SettingsUtils.java
+++ app/src/main/java/com/tsvietok/meteoradar/utils/SettingsUtils.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class SettingsUtils {
-    private static final String PREF_NAME = "com.tsvietok.meteoradar.dev.preferences";
+    private static final String PREF_NAME = "com.tsvietok.meteoradar.preferences";
 
     public static void saveIntSetting(Context context, String key, int value) {
         SharedPreferences.Editor editor = context.getSharedPreferences(PREF_NAME, 0).edit();
